e04c56ac45c5c0683385d5b007d7f7954f72adba
JustMangoT <minetuan2@gmail.com>
2021-12-15 08:11:26 +0700

    Update Upstream (Pufferfish)

--- settings.gradle.kts
+++
@@ -1,9 +0,0 @@
-pluginManagement {
-    repositories {
-        gradlePluginPortal()
-        maven("https://papermc.io/repo/repository/maven-public/")
-    }
-}
-
-rootProject.name = "ateliermc"
-include("ateliermc-api", "ateliermc-server")
